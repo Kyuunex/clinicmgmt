e86ee45daf6e2619d7fdce871178fadc4616e82f
Kyuunex <kyuunex@protonmail.ch>
2023-10-29 12:35:48 +0400

    update flask

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     package_data={'clinicmgmt': ['static/*', 'templates/*']},
     version="2023.08.24",
     install_requires=[
-        'Flask==2.3.3',
+        'Flask==3.0.0',
     ],
 )
